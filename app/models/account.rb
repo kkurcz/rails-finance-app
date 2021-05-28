e8f3a9ca9bf76862ba380ff6947a5e177146f867
Kevin Kurczseitz <kevinkurczseitz@icloud.com>
2021-05-28 15:27:17 +0800

    add validation

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,7 @@
 class Account < ApplicationRecord
+  has_many :account_updates
+
+  validates :name, presence: true, length: { minimum: 1 }
+
+  # shows currency of transaction (currently set to USD)
 end
