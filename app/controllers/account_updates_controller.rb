44c9844661d639d7a5f5e8ed3ca9713ded543b2a
Kevin Kurczseitz <kevinkurczseitz@icloud.com>
2021-05-28 15:24:36 +0800

    add index function

--- app/controllers/account_updates_controller.rb
+++ app/controllers/account_updates_controller.rb
@@ -1,2 +1,5 @@
 class AccountUpdatesController < ApplicationController
+  def index
+    @account_updates = AccountUpdate.all
+  end
 end
--- app/models/account_update.rb
+++ app/models/account_update.rb
@@ -1,5 +1,3 @@
 class AccountUpdate < ApplicationRecord
-  def index
-    @account_updates = AccountUpdate.all
-  end
+
 end
