4bc8212cf457192b5ec5f12bb121b3ae28ffa682
Kevin Kurczseitz <kevinkurczseitz@icloud.com>
2021-05-28 15:34:40 +0800

    add migration

--- db/migrate/20210528072146_create_account_updates.rb
+++ db/migrate/20210528072146_create_account_updates.rb
@@ -1,7 +1,8 @@
 class CreateAccountUpdates < ActiveRecord::Migration[6.0]
   def change
     create_table :account_updates do |t|
-
+      t.integer :balance
+      t.string :currency
       t.timestamps
     end
   end
