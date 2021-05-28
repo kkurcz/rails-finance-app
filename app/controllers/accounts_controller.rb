b8c0c86f512821645b1819b4bff5e46602920d35
Kevin Kurczseitz <kevinkurczseitz@icloud.com>
2021-05-28 17:24:31 +0800

    add show

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,4 +1,6 @@
 class AccountsController < ApplicationController
+  before_action :set_account, only: [:show] #:edit, :update, :destroy]
+
   def index
     @accounts = Account.all
     @account_updates = AccountUpdate.all
@@ -8,4 +10,21 @@ class AccountsController < ApplicationController
     # show recent transactions on users homepage
     # @transactions = policy_scope(Transaction).where(sender_wallet: @wallets).order(created_at: :desc)
   end
+
+  def show
+    # show all transactions of a given wallet
+    @account_updates = AccountUpdate.where(account_id: @account.id).order(created_at: :desc)
+    # @account_updates = policy_scope(Transaction).where(sender_wallet: @wallet.id).order(created_at: :desc)
+  end
+
+  private
+  def set_account
+    @account = Account.find(params[:id])
+    # authorize @wallet
+  end
+
+  def account_params
+    params.require(:account).permit(:name)
+  end
 end
+
