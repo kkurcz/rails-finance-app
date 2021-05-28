22b76ca850022d1442ac73c2bad33ced3d43dedc
Kevin Kurczseitz <kevinkurczseitz@icloud.com>
2021-05-28 15:32:04 +0800

    add validations

--- app/models/account_update.rb
+++ app/models/account_update.rb
@@ -1,3 +1,10 @@
 class AccountUpdate < ApplicationRecord
+  belongs_to :account
 
+  validates :month, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :balance, presence: true, numericality: { only_integer: true }
+  validates :currency, inclusion: { in: ["USD", "EURO", "CNY"] }
+
+  # shows currency of transaction
+  monetize :price_cents
 end
