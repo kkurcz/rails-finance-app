8b8507a4fe64078f80a21999b5fa635a0ec8d0e0
Kevin Kurczseitz <kevinkurczseitz@icloud.com>
2021-05-28 16:45:34 +0800

    add accounts index

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,4 +1,11 @@
 class AccountsController < ApplicationController
   def index
+    @accounts = Account.all
+    @account_updates = AccountUpdate.all
+
+    # @wallets = policy_scope(Wallet).order(created_at: :desc)
+
+    # show recent transactions on users homepage
+    # @transactions = policy_scope(Transaction).where(sender_wallet: @wallets).order(created_at: :desc)
   end
 end
